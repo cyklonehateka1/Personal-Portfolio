39657d51420560351771b099d41b536e75cf51a7
Emmanuel Hateka <emmanuelhateka.com>
2022-06-24 20:59:17 +0000

    deleted serverside folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import About from "./components/About";
-import NavBar from "./components/NavBar";
 import SocialLinks from "./components/SocialLinks";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <NavBar />
       <Home />
       <About />
       <SocialLinks />
